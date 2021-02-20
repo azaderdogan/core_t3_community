3ce2b8c8d2884a6a178476169f48273ccee61705
azaderdogan <nihatazad56@gmail.com>
2021-02-20 21:28:01 +0300

    setting dosyası düzenlendi

--- core_t3/settings/production.py
+++ core_t3/settings/production.py
@@ -14,3 +14,7 @@ DATABASES = {
 }
 ALLOWED_HOSTS = ['aeyazilimblogu.com', 'www.aeyazilimblogu.com','188.166.3.250']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
