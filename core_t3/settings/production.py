7a0e4b3364d67e7e56372eeca70f5dd0d53e570f
azaderdogan <nihatazad56@gmail.com>
2021-02-20 19:23:07 +0300

    setting dosyası düzenlendi

--- core_t3/settings/production.py
+++ core_t3/settings/production.py
@@ -13,4 +13,4 @@ DATABASES = {
     }
 }
 ALLOWED_HOSTS = ['aeyazilimblogu.com', 'www.aeyazilimblogu.com']
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
